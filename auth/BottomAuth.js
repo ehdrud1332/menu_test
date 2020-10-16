0567f8854998b950361669282b94f6fd03241177
CastleJaeMin <ehdrud1332@gmail.com>
2020-10-17 00:28:14 +0900

    BottonTab Icon Update

--- auth/BottomAuth.js
+++ auth/BottomAuth.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useLayoutEffect} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Ionicons} from '@expo/vector-icons';
+
 
 import MainScreen from "../screens/MainScreen";
 import ProfileScreen from "../screens/ProfileScreen";
@@ -20,10 +22,52 @@ const BottomAuth = ({navigation, route}) => {
     })
 
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Main" component={MainScreen} />
-            <Tab.Screen name="Search" component={SearchScreen} />
-            <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Navigator
+            tabBarOptions={{
+                //누를때=active 색갈
+                activeTintColor: 'black',
+                labelStyle: {fontWeight: '400', textTransform: 'uppercase'}
+            }}
+        >
+            <Tab.Screen
+                name="Main"
+                component={MainScreen}
+                options={{
+                    tabBarIcon: ({focused}) => (
+                        <Ionicons
+                            name="ios-home"
+                            color= {focused ? "black" : "#5a5a5a"}
+                            size={26}
+                        />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="Search"
+                component={SearchScreen}
+                options={{
+                    tabBarIcon: ({focused}) => (
+                        <Ionicons
+                            name="ios-search"
+                            color= {focused ? "black" : "#5a5a5a"}
+                            size={26}
+                        />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={ProfileScreen}
+                options={{
+                    tabBarIcon: ({focused}) => (
+                        <Ionicons
+                            name="md-person"
+                            color= {focused ? "black" : "#5a5a5a"}
+                            size={26}
+                        />
+                    )
+                }}
+            />
         </Tab.Navigator>
     );
 };
